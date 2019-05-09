a33f7eade93f48d63efff2f9398e8aee700c3bf6
juri200405 <juri200405@gmail.com>
2019-05-09 20:26:48 +0900

    文の分かち書きまで完了

--- src/tomioka/data.py
+++ src/tomioka/data.py
@@ -1,4 +1,32 @@
 import MeCab
+import pickle
+from pathlib import Path
+import csv
+import re
+import unicodedata
 
-m = MeCab.Tagger("-O wakati")
-print(m.parse("本日は晴天なり"))
+
+START_TOKEN = "<s>"
+END_TOKEN = "</s>"
+PAD_TOKEN = "<pad>"
+UNK_TOKEN = "<unk>"
+
+def normalize_string(s):
+    s = ''.join(c for c in unicodedata.normalize('NFKC', s) if unicodedata.category(c) != 'Mn')
+    s = re.sub(r'[\r\n]+', r'\n', s).strip()
+    return s
+
+def preprocess(me, sample):
+    tokens = ' 。 '.join(me.parse(s.strip()).strip() for s in re.split(r'[。\n]+', normalize_string(sample["data"])))
+    return {"tokens": tokens, "label": int(sample["label"])}
+
+def build(dpath, savedir):
+    data = []
+    with open(str(dpath / "train.csv"), 'rt', newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        me = MeCab.Tagger("-O wakati")
+        data = [preprocess(me, row) for row in reader]
+    print(data[0])
+
+if __name__ == "__main__":
+    build(Path("../../datas"), Path("../../datasets"))
