7a2b3dc03064a14c5272fed09b8ad01ee0655314
juri200405 <juri200405@gmail.com>
2019-05-13 23:19:15 +0900

    モデル構築部分の実装

--- src/tomioka/train.py
+++ src/tomioka/train.py
@@ -3,11 +3,12 @@ from pathlib import Path
 
 import numpy as np
 import torch
+import torch.nn as nn
+import torch.optim as optim
 
-#from src.tomioka.data import PAD_TOKEN
-#from tomioka.data import PAD_TOKEN
 from data import PAD_TOKEN
-#from baseline.data import PAD_TOKEN
+from dataloader import get_dataloader
+from model import Classifier
 
 def run(dataset_dir, hid_n=128, emb_size=128, batchsize=128, epoch=10, lr=0.01, use_cuda=False, seed=0):
     torch.cuda.manual_seed(seed)
@@ -23,6 +24,17 @@ def run(dataset_dir, hid_n=128, emb_size=128, batchsize=128, epoch=10, lr=0.01, 
     pad_index = token2index[PAD_TOKEN]
 
     fix_max_len = 50
+
+    train_dataloader = get_dataloader(train_dataset, batchsize, pad_index, fix_max_len=fix_max_len)
+
+    model = Classifier(voc_n, pad_index, hid_n, emb_size, dropout=0)
+    model = model.to(device)
+
+    optimizer = optim.Adam(model.parameters(), lr=lr)
+
+    loss_func = nn.CrossEntropyLoss()
+
+    model.train()
 
 if __name__ == "__main__":
     run(Path("./datas/datasets"))
