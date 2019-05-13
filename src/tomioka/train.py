7646c4c79b5e6c282530602b6c5e200e5568c397
juri200405 <juri200405@gmail.com>
2019-05-13 23:41:34 +0900

    train.py の学習部分の実装
    anaconda3にtqdmをインストールした

--- src/tomioka/train.py
+++ src/tomioka/train.py
@@ -2,6 +2,7 @@ import pickle
 from pathlib import Path
 
 import numpy as np
+from tqdm import tqdm
 import torch
 import torch.nn as nn
 import torch.optim as optim
@@ -35,6 +36,24 @@ def run(dataset_dir, hid_n=128, emb_size=128, batchsize=128, epoch=10, lr=0.01, 
     loss_func = nn.CrossEntropyLoss()
 
     model.train()
+
+    for i_epoch in tqdm(range(1, epoch + 1), total=epoch):
+        losses = []
+        for labels, inputs in train_dataloader:
+            labels = labels.to(device)
+            inputs = labels.to(device)
+
+            hid = model.init_hidden(inputs)
+            hid = hid.to(device)
+
+            out = model(inputs, hid)
+            loss = loss_func(out, labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            losses.append(loss.item())
+        tqdm.write(str(np.mean(losses)))
 
 if __name__ == "__main__":
     run(Path("./datas/datasets"))
